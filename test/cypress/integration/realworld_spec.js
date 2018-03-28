e009ad479493799891ba0ee76c158fc90976120b
Tor Hovland <teh@powel.com>
2018-03-28 13:16:10 +0200

    Opens an article.

--- test/cypress/integration/realworld_spec.js
+++ test/cypress/integration/realworld_spec.js
@@ -1,7 +1,15 @@
 describe('Home page', function () {
-    it("Verifies that the home page loads", function () {
+    it("Opens an article", function () {
         cy.visit('/')
+        cy.get('.article-preview h1').first().then(($title) => {
+            // Find title of first article
+            const title = $title.text()
 
-        cy.contains('Read more')
+            // Click on it
+            $title.click()
+
+            // Verify that we find the article title on the new page
+            cy.contains(title)
+        })
     })
 })

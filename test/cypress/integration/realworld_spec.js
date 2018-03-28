5a01284e617fcb159859d5d769845dbaac905545
Tor Hovland <teh@powel.com>
2018-03-28 12:30:06 +0200

    Running tests against local version as well as https://react-redux.realworld.io for reference.

--- test/cypress/integration/realworld_spec.js
+++ test/cypress/integration/realworld_spec.js
@@ -1,6 +1,6 @@
 describe('Home page', function () {
     it("Verifies that the home page loads", function () {
-        cy.visit('https://react-redux.realworld.io')
+        cy.visit('/')
 
         cy.contains('Read more')
     })
